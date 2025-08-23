d9e08277ccd1b4c228e99ac445c799ab8e250af9
Alastair Douglas <altreus@altre.us>
2025-08-23 17:36:11 +0100

    This seems to work...

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 extern crate sdl2;
 
+use std::f64::consts::TAU;
 use sdl2::rect::Point;
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use std::time::{Duration, Instant};
+use std::time::Instant;
 
 const SCREEN_WIDTH: u32 = 160;
 const SCREEN_HEIGHT: u32 = 120;
 const PIXEL_SCALE: u32 = 4;
 const FRAME_RATE: u32 = 20;
 const MS_PER_FRAME: u32 = 1000 / FRAME_RATE;
+const FIELD_OF_VIEW : i32 = 90;
 
 // TODO: This could be an analogue angle but we're not doing that for
 // this toy project.
@@ -26,14 +28,55 @@ enum MovementDirection {
     ForwardLeft
 }
 
+#[derive(Debug)]
 enum Rotation {
     Left, Right
+}
+
+#[derive(Default, Debug)]
+struct Point3 {
+    pub x: i32,
+    pub y: i32,
+    pub z: i32
+}
+
+impl Point3 {
+    fn new(x: i32, y: i32, z: i32) -> Point3 {
+        Point3{ x,y,z }
+    }
+}
+
+#[derive(Default, Debug)]
+struct FPoint3 {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64
+}
+
+impl FPoint3 {
+    fn new(x: f64, y: f64, z: f64) -> FPoint3 {
+        FPoint3{ x,y,z }
+    }
 }
 
 #[derive(Default)]
 struct PlayerMovement {
     pub movement: Option<MovementDirection>,
     pub rotation: Option<Rotation>
+}
+
+#[derive(Default)]
+struct Player {
+    pub location: Point3,
+    pub angle: f64,
+    pub movement: PlayerMovement,
+}
+
+// In Doom, all points in the map are integers, so we don't need to use
+// FPoint3 to define a wall; only to avoid losing precision while rendering it
+struct Wall {
+    pub line: (Point3, Point3),
+    pub height: i32
 }
 
 fn main() {
@@ -61,7 +104,8 @@ fn main() {
     canvas.present();
 
     // TODO: Expand this to a game context object
-    let mut player_movement : PlayerMovement = Default::default();
+    let mut player : Player = Default::default();
+    player.location.y = 20;
 
     let mut event_pump = sdl.event_pump().unwrap();
     let mut lastframe = Instant::now();
@@ -69,16 +113,16 @@ fn main() {
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} => break 'main,
-                Event::KeyDown { keycode: Some(k), .. } => on_key_down(&mut player_movement, k),
-                Event::KeyUp   { keycode: Some(k), .. } => on_key_up  (&mut player_movement, k),
+                Event::KeyDown { keycode: Some(k), .. } => on_key_down(&mut player.movement, k),
+                Event::KeyUp   { keycode: Some(k), .. } => on_key_up  (&mut player.movement, k),
                 _ => {}
             }
         }
 
-        move_player(&player_movement);
         // Draw frame_rate times per second
         if lastframe.elapsed().as_millis() >= MS_PER_FRAME as u128 {
-            draw_stuff(&mut canvas);
+            move_player(&mut player);
+            draw_stuff(&mut canvas, &player);
 
             canvas.present();
             lastframe = Instant::now();
@@ -86,27 +130,94 @@ fn main() {
     }
 }
 
-fn move_player(p: &PlayerMovement) {
-    match &p.movement {
+fn move_player(p: &mut Player) {
+    // Let's Program Doom used 4 degrees, which is 1/90 of a turn
+    match &p.movement.rotation {
+        None => {},
+        Some(Rotation::Left) => { p.angle -= TAU/90.0; if p.angle < 0.0 { p.angle += TAU } }
+        Some(Rotation::Right) => { p.angle += TAU/90.0; if p.angle > TAU { p.angle -= TAU } }
+    }
+
+    let dx = (p.angle.sin() * 10.0) as i32;
+    let dz = (p.angle.cos() * 10.0) as i32;
+    println!("Player delta xz: {:?} {:?}", dx, dz);
+
+    match &p.movement.movement {
         None => {}
-        Some(x) => { println!("{:?}", x) }
+        Some(x) => { match x {
+            // Cargo-culted from the video, in which he seems very confident it
+            // works like this
+            MovementDirection::Forward  => { p.location.x += dx; p.location.z += dz },
+            MovementDirection::Backward => { p.location.x -= dx; p.location.z -= dz },
+            MovementDirection::Right    => { p.location.x += dz; p.location.z += dx },
+            MovementDirection::Left     => { p.location.x -= dz; p.location.z -= dx },
+            _ => {},
+            // Can't be bothered figuring this out yet. It's going to involve
+            // a point on a circle between the two directions and therefore
+            // more sines and cosines
+            //MovementDirection::ForwardRight,
+            //MovementDirection::BackwardRight,
+            //MovementDirection::BackwardLeft,
+            //MovementDirection::ForwardLeft
+        }}
     }
 }
 
-fn draw_stuff(canvas: &mut sdl2::render::WindowCanvas) {
-    canvas.set_draw_color(Color::WHITE);
+fn draw_stuff(canvas: &mut sdl2::render::WindowCanvas, player: &Player) {
+    canvas.set_draw_color(Color::BLUE);
     canvas.clear();
 
-    for x in 0 .. (SCREEN_WIDTH / 2) - 1 {
-        for y in 0 .. (SCREEN_HEIGHT / 2) - 1 {
-            let x_pc : f32 = x as f32 / (SCREEN_WIDTH as f32 / 2.0);
-            let y_pc : f32 = y as f32 / (SCREEN_HEIGHT as f32 / 2.0);
+    // In the video he defines a wall as 4 points in 3D, and then constructs
+    // them programmatically from something or other. Let's not do that,
+    // because he then later just draws one point, and then two, and then a
+    // line in between them.
+    // Walls in Doom are coded as a line with height, so let's start with a
+    // line and later add the height.
 
-            draw_pixel(canvas, Point::new(x as _,y as _), Color::RGB(
-                (255.0 * x_pc) as _, (255.0 * y_pc) as _, (255.0 * x_pc * y_pc ) as _ )
-            );
-        }
-    }
+    let wall = Wall {
+        line: ( Point3::new(0, 0, 50), Point3::new(50, 0, 50) ),
+        height: 40
+    };
+
+    println!("Wall's zeroth point is {:?}", wall.line.0);
+    // We'll draw just the first point of the wall's line to begin with
+    // First we'll take a copy of the point and adjust it by the position
+    // of the player. TODO: We can implement maths on the struct
+    // First we translate the point by the player's location
+    let mut point_to_draw = FPoint3::new(
+        (wall.line.0.x - player.location.x) as _,
+        (wall.line.0.y - player.location.y) as _,
+        (wall.line.0.z - player.location.z) as _
+    );
+    println!("After translation {:?}", point_to_draw);
+    // Now the player is at 0,0,0 we can rotate the world by their angle
+    point_to_draw.x = point_to_draw.x * player.angle.cos() - point_to_draw.z * player.angle.sin();
+    point_to_draw.z = point_to_draw.z * player.angle.cos() + point_to_draw.x * player.angle.sin();
+    println!("After rotation {:?}", point_to_draw);
+
+    if point_to_draw.z <= 0.0 { return }
+
+    // The pixel to draw is brought into the centre of the screen based on the
+    // distance, which is its Z value after these transformations. In the video
+    // he simply divides the 2D coordinates by the depth coordinate, and then
+    // multiplies by some arbitrary field of view value.
+    let pixel = Point::new(
+        ((point_to_draw.x / point_to_draw.z * FIELD_OF_VIEW as f64) + (SCREEN_WIDTH as f64 / 2.0)) as i32,
+        // I think 0,0 turned out to be the top left as we expect.
+        ((point_to_draw.y / point_to_draw.z * FIELD_OF_VIEW as f64) + (SCREEN_HEIGHT as f64 / 2.0)) as i32,
+    );
+    println!("After projection {:?}", pixel);
+    draw_pixel( canvas, pixel, Color::YELLOW );
+    // for x in 0 .. (SCREEN_WIDTH / 2) - 1 {
+    //     for y in 0 .. (SCREEN_HEIGHT / 2) - 1 {
+    //         let x_pc : f32 = x as f32 / (SCREEN_WIDTH as f32 / 2.0);
+    //         let y_pc : f32 = y as f32 / (SCREEN_HEIGHT as f32 / 2.0);
+    //
+    //         draw_pixel(canvas, Point::new(x as _,y as _), Color::RGB(
+    //             (255.0 * x_pc) as _, (255.0 * y_pc) as _, (255.0 * x_pc * y_pc ) as _ )
+    //         );
+    //     }
+    // }
 }
 
 fn draw_pixel(canvas: &mut sdl2::render::WindowCanvas, p: Point, c: Color) {
@@ -199,8 +310,14 @@ fn on_key_up(p: &mut PlayerMovement, k: Keycode) {
             }
         },
 
-        Keycode::LEFT if !p.rotation.is_some() => p.rotation = Some(Rotation::Left),
-        Keycode::RIGHT if !p.rotation.is_some() => p.rotation = Some(Rotation::Right),
+        Keycode::LEFT => match p.rotation {
+            Some(Rotation::Left) => p.rotation = None,
+            _ => {}
+        },
+        Keycode::RIGHT => match p.rotation {
+            Some(Rotation::Right) => p.rotation = None,
+            _ => {}
+        },
         _ => {}
     }
 }
