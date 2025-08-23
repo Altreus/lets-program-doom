af7ad7cc7c26caee08411c8b25987f6106e5cdf9
Alastair Douglas <altreus@altre.us>
2025-08-23 21:03:09 +0100

    Add draw_wall, draw bottom line

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate sdl2;
 
 use std::f64::consts::TAU;
 use sdl2::rect::Point;
+use sdl2::render::WindowCanvas;
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
@@ -75,6 +76,9 @@ struct Player {
 // In Doom, all points in the map are integers, so we don't need to use
 // FPoint3 to define a wall; only to avoid losing precision while rendering it
 struct Wall {
+    // FIXME: The Y value of both points must be the same, so we shouldn't
+    // use Point3 at all, here. We should use Point2 and remember the local Y
+    // represents world Z
     pub line: [Point3;2],
     pub height: i32
 }
@@ -131,6 +135,8 @@ fn main() {
 }
 
 fn move_player(p: &mut Player) {
+    if p.movement.movement.is_none() && p.movement.rotation.is_none() { return }
+
     // Let's Program Doom used 4 degrees, which is 1/90 of a turn
     match &p.movement.rotation {
         None => {},
@@ -138,10 +144,14 @@ fn move_player(p: &mut Player) {
         Some(Rotation::Right) => { p.angle += TAU/90.0; if p.angle > TAU { p.angle -= TAU } }
     }
 
+    // We're trying to move by 10 units in the direction of the player
+    // (or perpendicular to it) but something's wrong
     let dx = (p.angle.sin() * 10.0) as i32;
     let dz = (p.angle.cos() * 10.0) as i32;
-    println!("Player delta xz: {:?} {:?}", dx, dz);
 
+    println!("Player angle and x/z movement deltas: {:?} {:?} {:?}",
+        p.angle.to_degrees(), dx, dz);
+    println!("Player location before move: {:?}", p.location);
     match &p.movement.movement {
         None => {}
         Some(x) => { match x {
@@ -149,8 +159,8 @@ fn move_player(p: &mut Player) {
             // works like this
             MovementDirection::Forward  => { p.location.x += dx; p.location.z += dz },
             MovementDirection::Backward => { p.location.x -= dx; p.location.z -= dz },
-            MovementDirection::Right    => { p.location.x += dz; p.location.z += dx },
-            MovementDirection::Left     => { p.location.x -= dz; p.location.z -= dx },
+            MovementDirection::Right    => { p.location.x += dz; p.location.z -= dx },
+            MovementDirection::Left     => { p.location.x -= dz; p.location.z += dx },
             _ => {},
             // Can't be bothered figuring this out yet. It's going to involve
             // a point on a circle between the two directions and therefore
@@ -161,9 +171,10 @@ fn move_player(p: &mut Player) {
             //MovementDirection::ForwardLeft
         }}
     }
+    println!("Player location after move: {:?}", p.location);
 }
 
-fn draw_stuff(canvas: &mut sdl2::render::WindowCanvas, player: &Player) {
+fn draw_stuff(canvas: &mut WindowCanvas, player: &Player) {
     canvas.set_draw_color(Color::BLUE);
     canvas.clear();
 
@@ -175,36 +186,12 @@ fn draw_stuff(canvas: &mut sdl2::render::WindowCanvas, player: &Player) {
     // line and later add the height.
 
     let wall = Wall {
-        line: [ Point3::new(0, 0, 50), Point3::new(50, 0, 50) ],
+        line: [ Point3::new(-10, 0, 50), Point3::new(10, 0, 50) ],
         height: 40
     };
 
-    for point in wall.line {
-        println!("Point is {:?}", point);
-        let mut point_to_draw = FPoint3::new(
-            (point.x - player.location.x) as _,
-            (point.y - player.location.y) as _,
-            (point.z - player.location.z) as _
-        );
-        println!("After translation {:?}", point_to_draw);
+    draw_wall( canvas, player, wall, Color::YELLOW );
 
-        let newx = point_to_draw.x * player.angle.cos() - point_to_draw.z * player.angle.sin();
-        let newz = point_to_draw.z * player.angle.cos() + point_to_draw.x * player.angle.sin();
-        point_to_draw.x = newx; point_to_draw.z = newz;
-        println!("After rotation {:?}", point_to_draw);
-
-        if point_to_draw.z <= 0.0 { return }
-
-        // Invert the Y coordinate. World coordinates have +Y is up, but
-        // screen coordinates have +Y is down. That'll flip the world, so
-        // flip it back
-        let pixel = Point::new(
-            ((point_to_draw.x / point_to_draw.z * FIELD_OF_VIEW as f64) + (SCREEN_WIDTH as f64 / 2.0)) as i32,
-            ((-point_to_draw.y / point_to_draw.z * FIELD_OF_VIEW as f64) + (SCREEN_HEIGHT as f64 / 2.0)) as i32,
-        );
-        println!("After projection {:?}", pixel);
-        draw_pixel( canvas, pixel, Color::YELLOW );
-    }
     // for x in 0 .. (SCREEN_WIDTH / 2) - 1 {
     //     for y in 0 .. (SCREEN_HEIGHT / 2) - 1 {
     //         let x_pc : f32 = x as f32 / (SCREEN_WIDTH as f32 / 2.0);
@@ -217,7 +204,69 @@ fn draw_stuff(canvas: &mut sdl2::render::WindowCanvas, player: &Player) {
     // }
 }
 
-fn draw_pixel(canvas: &mut sdl2::render::WindowCanvas, p: Point, c: Color) {
+// Eventually the wall will know its colour, which will of course be a texture
+// Eventually the player wil be the whole game state.
+fn draw_wall(canvas: &mut WindowCanvas, player: &Player, wall: Wall, c: Color) {
+    // In the video the drawWall function iterates over the SCREEN values of
+    // the wall that he computes in the draw function. Maybe he tidies this up
+    // later but why would you write code you need to tidy up? The function is
+    // called drawWall so it should accept wall coordinates, which is what this
+    // function does.
+    //
+    // So we're going to take the two base coordinates of the wall and convert
+    // them to screen coordinates, and then draw all the pixels between them.
+    // We can use the basic line formula to do this by normalising the X and
+    // Y deltas of the line (we're in screen coordinates now).
+
+    let screen_point_1 = point3_to_point2( &wall.line[0], player );
+    let screen_point_2 = point3_to_point2( &wall.line[1], player );
+
+    let dx = screen_point_1.x - screen_point_2.x;
+    let dy = screen_point_1.y - screen_point_2.y;
+
+
+    for xval in screen_point_1.x .. screen_point_2.x {
+        // xval - wall.line[0].x / dx normalises xval so needs to be a float
+        // multiply that by dy and we get a normalised y value proportional to x
+        // make that an int and add the actual y value of the first point
+        let yval = ((dy * ( xval - screen_point_1.x ) ) as f64 / dx as f64 ) as i32 + screen_point_1.y;
+
+        draw_pixel( canvas, Point::new(xval, yval), c );
+    }
+}
+
+fn point3_to_point2(p3: &Point3, player: &Player) -> Point {
+    let mut translated_point = FPoint3::new(
+        (p3.x - player.location.x) as _,
+        (p3.y - player.location.y) as _,
+        (p3.z - player.location.z) as _
+    );
+    println!("Translated point {:?} to {:?} via {:?}", p3, translated_point, player.location);
+
+    let newx = translated_point.x * player.angle.cos() - translated_point.z * player.angle.sin();
+    let newz = translated_point.z * player.angle.cos() + translated_point.x * player.angle.sin();
+    translated_point.x = newx; translated_point.z = newz;
+
+    println!("Rotated point through {:?} radians ({:?} degrees) to {:?}",
+        player.angle, player.angle.to_degrees(), translated_point);
+
+    // FIXME: We can't just return None here and make this an Option function
+    // because we still need to interpolate between two points. For now we
+    // fudge the Z value so there's no DIV0 error and come back to clipping
+    // later
+    if translated_point.z == 0.0 { translated_point.z = 0.1 }
+    // Invert the Y coordinate. World coordinates have +Y is up, but
+    // screen coordinates have +Y is down. That'll flip the world, so
+    // flip it back
+    let pixel = Point::new(
+        ((translated_point.x / translated_point.z * FIELD_OF_VIEW as f64) + (SCREEN_WIDTH as f64 / 2.0)) as i32,
+        ((-translated_point.y / translated_point.z * FIELD_OF_VIEW as f64) + (SCREEN_HEIGHT as f64 / 2.0)) as i32,
+    );
+
+    return pixel;
+}
+
+fn draw_pixel(canvas: &mut WindowCanvas, p: Point, c: Color) {
     canvas.set_draw_color(c);
     canvas.draw_point(p).expect("Error drawing pixel :(");
 }
